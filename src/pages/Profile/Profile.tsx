744bb9322fa14375d0031c7584c70b3271c30e38
Valerian Ehret <66052889+valerianEhret@users.noreply.github.com>
2021-12-12 16:25:05 +0100

    Add redirect from profile page to login page (#2)
    User is redirected to the login page if user is not logged in.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -2,34 +2,40 @@ import React from 'react'
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../../redux/store";
 import {IUser, logoutTC} from "../Login/loginReducer";
+import {Preloader} from "../Login/Preloader";
+import {Redirect} from "react-router-dom";
+import {PATH} from "../Routes";
 
 
 function Profile() {
-const user = useSelector<AppStateType, IUser | null>(state=>state.auth.user)
-const dispatch = useDispatch()
+    const user = useSelector<AppStateType, IUser | null>(state => state.auth.user)
+    const isLoggedIn = useSelector<AppStateType, boolean>(state => state.auth.isLoggedIn)
+    const loading = useSelector<AppStateType, boolean>(state => state.auth.loading)
+    const dispatch = useDispatch()
 
-const logout = () => {
-    dispatch(logoutTC())
-}
+    const logout = () => {
+        dispatch(logoutTC())
+    }
 
-
-
+    if (!isLoggedIn) {
+        return <Redirect to={PATH.LOGIN}/>
+    }
     return (
         <div>
+            {loading && <Preloader/>}
             <div>
                 {
-                user !== null? <div>
-                    <img src={user.avatar} alt='avatar'/>
-                    <p/>
-                    Name: {user.name}
-                    <p/>
-                    E-Mail: {user.email}
-                    <p/>
-                </div> : 'No data'
-
+                    user !== null ? <div>
+                        <img src={user.avatar} alt='avatar'/>
+                        <p/>
+                        Name: {user.name}
+                        <p/>
+                        E-Mail: {user.email}
+                        <p/>
+                    </div> : 'No data'
                 }
             </div>
-            <button onClick={logout}>Logout</button>
+            <button onClick={logout} disabled={!isLoggedIn}>Logout</button>
         </div>
     )
 }
